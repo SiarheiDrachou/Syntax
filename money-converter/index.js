88680bb6e75d87601b6d1ae9a89116b668ce5cee
SiarheiDrachou <9kudza@tut.by>
2020-01-06 15:02:38 +0300

    исправления для index.js 1) поменял размер посроенной таблицы под любое количество валют получаемых при помощи API запроса 2) оставил записываться в localStorage только название валюты 3) вынес отдельно функцию обработчик для выпадающего меню с валютами 4) добавил проверку на клик по пустому месту меню 5)добавил подсветку для выбранной валюты 6) исправил проблему с сохранением выбранной валюты после перезагрузки страницы

--- money-converter/index.js
+++ money-converter/index.js
@@ -1,29 +1,35 @@
-localStorage.setItem('value', 'USD');
 let btnEl = document.querySelector(".btn");
 let theadEl = document.querySelector(".col");
 let tbodyEl = document.querySelector(".table-body");
 let optionEl = document.querySelector(".dropdown-menu");
 let spinnerEl = document.querySelector('.spinner-border');
 let tableEl = document.querySelector('.table');
-let arrValues = [];
-let arrKeys = [];
 spinnerEl.style.display = 'none';
 
-function buildTbody() {
+function startSession() {
+    if(localStorage.length == 0) {
+        localStorage.setItem('value', 'USD');
+        let elMenu = document.querySelector(`.${localStorage.getItem('value')}`);
+        elMenu.classList.add('active');
+    } 
+    else {
+        let elMenu = document.querySelector(`.${localStorage.getItem('value')}`);
+        elMenu.classList.add('active');
+    }
+    readMoney();
+}
+
+function buildTbody(arrKeys, arrValues) {
     btnEl.innerHTML = localStorage.getItem('value');
     theadEl.innerHTML = localStorage.getItem('value') + " Exchange rate";
     tbodyEl.innerHTML = '';
-    for(let i = 0; i < 52; i++) {
-        arrValues.push(localStorage.key(arrValues[i]));
-        arrKeys.push(localStorage.getItem(arrValues[i]));
+    for(let i = 0; i < arrValues.length; i++) {
         let tbodyTextEl =  `<tr class="row-${i+1}">
                                 <th scope="row"> ${arrValues[i]} </th>
                                 <td>${arrKeys[i]}</td>
                             </tr>`;
         tbodyEl.insertAdjacentHTML('beforeend', tbodyTextEl);
     };
-
-    buildThead()
 }
 
 async function readMoney () {
@@ -31,12 +37,9 @@ async function readMoney () {
         loader();
         let money = await fetch(`https://api.exchangerate-api.com/v4/latest/${localStorage.getItem('value')}`);
         let moneys = await money.json();
-        arrValues = Object.keys(moneys.rates);
-        arrKeys = Object.values(moneys.rates);
-        for(let j = 0; j < arrValues.length; j++) {
-            localStorage.setItem(arrValues[j],arrKeys[j]); 
-        };
-        buildTbody();
+        let arrValues = Object.keys(moneys.rates);
+        let arrKeys = Object.values(moneys.rates);
+        buildTbody(arrKeys, arrValues);
     }
     catch {
         alert("ERROR!!!")
@@ -46,23 +49,22 @@ async function readMoney () {
     }
 }
 
-function buildThead() {
-    optionEl.addEventListener('click', function(event) {
+function buildThead(event) {
+    if(event.target.textContent.length == 3) {
         localStorage.setItem('value', event.target.textContent);
-        let El = event.target;
+        let elMenu = event.target;
         for(let i = 0; i < optionEl.children.length; i++) {
             let linkEl = optionEl.children[i];
             linkEl.classList.remove('active');
         }
-        El.classList.add('active');
-        btnEl.innerHTML = localStorage.getItem('value');
-        theadEl.innerHTML = localStorage.getItem('value') + " Exchange rate";
+        elMenu.classList.add('active');
         readMoney();
-    });
+    }
 }
+optionEl.addEventListener('click', buildThead);
 
 function loader() {
     spinnerEl.style.display = 'flex';
 }
 
-readMoney();
+startSession();
